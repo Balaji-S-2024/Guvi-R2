8b23b007825932152af84148c10d93059ac75327
Balaji-S-2024 <balajisankar0202@gmail.com>
2023-12-20 23:07:30 +0530

    Updated link

--- frontend/Authentication-Module/src/components/goto.jsx
+++ frontend/Authentication-Module/src/components/goto.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function goto() {
   return (
-    <div>got to url http://localhost:5173/app</div>
+    <div>got to url <a href="http://localhost:5173/app">http://localhost:5173/app</a> </div>
   )
 }
 
